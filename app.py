87986617ef50f0c9e0bfa2dd2ee4dd9b292f0fd8
mantfish <david.obm@gmail.com>
2021-02-04 21:41:41 +0000

    finilized arduino codes, so just updating rpi stuff

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template, jsonify
 import serial
 import time
 
+# set percentage of max thrust for movement NOTE must be between 0 -> 0.5
+pThrust = 0.25*100
+rvDir = 1
+rhDir = 1
+lvDir = 1
+lhDir = 1
+
+lhCode = 350
+lvCode = 450
+rhCode = 550
+rvCode = 650
+
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 
@@ -15,78 +27,94 @@ def home():
 
 @app.route("/up")
 def up():
+    serialWrite(lvCode + lvDir*pThrust)
+    serialWrite(rvCode + rvDir*pThrust)
     print("up")
     return "recieved"
 
 @app.route("/down")
 def down():
+    serialWrite(lvCode + -1*lvDir*pThrust)
+    serialWrite(rvCode + -1*rvDir*pThrust)
     print("down")
     return "recieved"
 
 @app.route("/forward")
 def forward():
+    serialWrite(lvCode + 1*lhDir*pThrust)
+    serialWrite(rvCode + 1*rhDir*pThrust)
     print("forward")
     return "recieved"
 
 @app.route("/backward")
 def backward():
+    serialWrite(lvCode + -1*lhDir*pThrust)
+    serialWrite(rvCode + -1*rhDir*pThrust)
     print("backward")
     return "recieved"
 
 
 @app.route("/left")
 def left():
+    serialWrite(lvCode + -1*lhDir*pThrust)
+    serialWrite(rvCode + 1*rhDir*pThrust)
     print("left")
     return "recieved"
 
 
 @app.route("/right")
 def right():
+    serialWrite(lvCode + 1*lhDir*pThrust)
+    serialWrite(rvCode + -1*rhDir*pThrust)
     print("right")
     return "recieved"
 
 
 @app.route("/panUp")
 def panUp():
-    serialWrite('b')
+    serialWrite(700)
     print("panUp")
     return "recieved"
 
 @app.route("/panDown")
 def panDown():
-    serialWrite('a')
+    serialWrite(710)
     print("panDown")
     return "recieved"
 
 
 @app.route("/stop")
 def stop():
+    serialWrite(rhCode)
+    serialWrite(rvCode)
+    serialWrite(lvCode)
+    serialWrite(lhCode)
     print("stop")
-    return "recieved"
-
-
-@app.route("/stopPan")
-def stopPan():
-    print("stopPan")
     return "recieved"
 
 @app.route("/stopHorizontal")
 def stopHorizontal():
+    serialWrite(lhCode)
+    serialWrite(rhCode)
     print("stopH")
     return "recieved"
 
 @app.route("/stopVertical")
 def stopVertical():
+    serialWrite(rvCode)
+    serialWrite(lvCode)
     print("stopV")
     return "recieved"
 
 @app.route("/on")
 def lightsOn():
+    serialWrite(201)
     print("On")
     return "recieved"
 
 @app.route("/off")
 def lightsOff():
+    serialWrite(200)
     print("Off")
     return "recieved"
 
@@ -94,16 +122,23 @@ def lightsOff():
 
 @app.route("/left/<value>")
 def leftThruster(value):
+    value = int(value)
+    serialWrite(lhCode+value*0.5*100)
     print("left "+ value)
     return "recieved"
 
 @app.route("/right/<value>")
 def rightThruster(value):
+    value = int(value)
+    serialWrite(rhCode+value*0.5*100)
     print("right " + value)
     return "recieved"
 
 @app.route("/vertical/<value>")
 def vertical(value):
+    value = int(value)
+    serialWrite(lvCode+value*0.5*100)
+    serialWrite(rvCode+value*0.5*100)
     print("Vertical "+ value)
     return "recieved"
 
@@ -115,6 +150,9 @@ def updateValues():
     return jsonify(roll=roll,pitch = pitch)
 
 def serialWrite(towrite):
+    towrite = str(towrite)
+    towrite += "\n"
+    towrite = towrite.encode('utf-8')
 
     if isConnected == True:
         ser.write(towrite.encode())
@@ -123,16 +161,15 @@ def request_values():
     if isConnected == False:
         roll = 0
         pitch = 0
-        yaw = 0
     else:
-        ser.write(b"1\n")
+        ser.write(120)
         if ser.in_waiting > 0:
             roll = ser.readline().decode('utf-8').rstrip()
             print(roll)
         else:
             roll = 0
 
-        ser.write(b"2\n")
+        ser.write(121)
         if ser.in_waiting > 0:
             pitch = ser.readline().decode('utf-8').rstrip()
             print(pitch)
